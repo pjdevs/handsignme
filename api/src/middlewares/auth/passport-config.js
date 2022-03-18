14e3e81b2887b1195259c26bb38d3e56c460f252
BRASSART Mathieu <Mathieu.Brassart@bordeaux-inp.fr>
2022-03-18 20:07:51 +0100

    passport

---
+++ api/src/middlewares/auth/passport-config.js
@@ -0,0 +1,26 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+function initialize(passport, getUserByEmail, getUserById) {
+   const authenticateUser = async (email, passport, done) => {
+      const user = getUserByEmail(email);
+      if (user == null) {
+      return done(null, false, {message: 'No user with that email'});
+      }
+      try {
+         if (await bcrypt.compare(passport, user.password)) {
+            return done(null, user);
+         } else {
+            return done(null, false, {message: 'Password incorrect'});
+         }
+      } catch(e) {
+         return done(e);
+      }
+
+   }
+   passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+   passport.serializeUser((user, done) => done(null, user.id));
+   passport.deserializeUser((id, done) => done(null, getUserById(id))); 
+}
+
+module.exports = initialize;
