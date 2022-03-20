dde9964ad113da1f1d860ed0d4af97b0b7e3b141
GassaraHEDI <hedi.gassara2@gmail.com>
2022-03-20 16:02:12 +0100

    la nouvelle version de frontend

---
+++ FrontEnd/src/components/Footers/FooterSmall.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer
+    class="pb-6"
+    :class="[
+      absolute ? 'absolute w-full bottom-0 bg-blueGray-800' : 'relative',
+    ]"
+  >
+    <div class="container mx-auto px-4">
+      <hr class="mb-6 border-b-1 border-blueGray-600" />
+        <div
+        class="flex flex-wrap items-center md:justify-between justify-center"
+      >
+        <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+          <div class="text-sm text-blueGray-500 font-semibold py-1">
+            Copyright © {{ date }}  by
+            <a
+              href="https://www.creative-tim.com?ref=vn-footer"
+              class="text-blueGray-500 hover:text-blueGray-800"
+            >
+              HandSignMe Team
+            </a>
+            .
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    };
+  },
+  props: {
+    absolute: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
