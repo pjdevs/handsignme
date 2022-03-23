bb28fe4f2ede7695d5574dd0386c80694fc10cd1
pjdevs <pj33720@gmail.com>
2022-03-23 17:11:37 +0100

    First working test

--- api/tests/unit/controllerCommon.js
+++ api/tests/unit/controllerCommon.js
@@ -2,15 +2,16 @@ const { mockResponse, mockRequest } = require('jest-mock-req-res')
 const db = require('../../src/models')
 const config = require('../../src/config')
 
-module.exports = () => {
+module.exports = (() => {
     const req = mockRequest()
     const res = mockResponse()
 
-    req.app.settings.db = db
-    req.app.settings.config = config
+    db.sequelize.sync({ force: true })
 
-    return {
-        req: req,
-        res: res
+    req.app.settings = {
+        db: db,
+        config: config
     }
-}
+
+    return { req, res }
+})()
