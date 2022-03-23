794d8ce0310fca1d7f992c52a6bea78b6f6c0905
pjdevs <pj33720@gmail.com>
2022-03-23 08:49:40 +0100

    Fix sonar error and added body-parser to parse req

--- api/src/index.js
+++ api/src/index.js
@@ -3,17 +3,22 @@ const routes = require('./routes')
 const db = require('./models')
 const config = require('./config')
 const multer = require('multer')
+const bodyParser = require('body-parser')
 
 const storage = multer.memoryStorage()
-const upload = multer({ storage: storage,
+const upload = multer({
+    storage: storage,
     limits: {
         fieldNameSize: 100,
-        fileSize: 1048576
-    } })
+        fileSize: 8000000
+    }
+})
 
 const app = express()
 app.disable('x-powered-by')
 
+app.use('/api', bodyParser.json({ limit: '2mb' }))
+app.use('/api', bodyParser.urlencoded({ extended: false, limit: '2mb' }))
 app.use('/api', routes(db, config, { upload }))
 
 const server = app.listen(config.server.port, config.server.host, () => {
