2bea5f5a067f84b87a5226eee49df27a4f0cf523
pjdevs <pj33720@gmail.com>
2022-03-30 09:49:50 +0200

    Fix hash for test

--- api/src/utils/hash.js
+++ api/src/utils/hash.js
@@ -3,12 +3,16 @@ const crypto = require('crypto')
 const config = require('../config')
 
 function hashFile(filename) {
-    const buffer = fs.readFileSync(`${config.storage.filesPath}/${filename}`)
+    try {
+        const buffer = fs.readFileSync(`${config.storage.filesPath}/${filename}`)
 
-    const md5Hasher = crypto.createHash('md5')
-    const hash = md5Hasher.update(buffer).digest('hex')
+        const md5Hasher = crypto.createHash('md5')
+        const hash = md5Hasher.update(buffer).digest('hex')
 
-    return hash
+        return hash
+    } catch (err) {
+        return ''
+    }
 }
 
 module.exports = {
