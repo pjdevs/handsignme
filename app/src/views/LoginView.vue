e9d93563855311af7a825f0811d37022b414e4a2
GassaraHEDI <hedi.gassara2@gmail.com>
2022-04-09 13:51:42 +0200

    front login/signup pages

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,19 +1,40 @@
 <template>
-
-<div class="container">
-  <form ref="loginForm" class="form-signin" @submit.prevent="login">
-    <h2 class="form-signin-heading">Please log in</h2>
-    <div v-if="err" class="alert alert-danger">
-        {{err}}
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <div class="container">
+          <form ref="loginForm" class="form-signin" @submit.prevent="login">
+            <h2 class="form-signin-heading">Please log in</h2>
+            <div v-if="err" class="alert alert-danger">
+                {{err}}
+            </div>
+            <!-- Mail input -->
+            <div class="form-outline mb-4">
+              <input v-model="email" type="text" id="inputUsername" name="email" class="form-control" placeholder="Email" required autofocus>
+              <label for="inputUsername" class="sr-only">Email address</label>
+            </div>
+            <!-- Password input -->
+            <div class="form-outline mb-3">
+              <input v-model="password" type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+              <label for="inputPassword" class="sr-only">Password</label>
+            </div>
+            <div class="text-center text-lg-start mt-4 pt-2">
+              <button class="btn btn-primary btn-lg"
+                style="padding-left: 2.5rem; padding-right: 2.5rem;" type="submit">Log in</button>
+              <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/app/signup"
+                  class="link-danger">Register</a></p>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-    <label for="inputUsername" class="sr-only">Email</label>
-    <input v-model="email" type="text" id="inputUsername" name="email" class="form-control" placeholder="Email" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input v-model="password" type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
-    <a href="/app/signup" class="btn btn-lg btn-primary btn-block m-2">Register</a>
-  </form>
-</div>
+  </div>
+</section>
 </template>
 
 <script>
@@ -46,3 +67,20 @@ export default {
   }
 }
 </script>
+<style scoped>
+.divider:after,
+.divider:before {
+content: "";
+flex: 1;
+height: 1px;
+background: #eee;
+}
+.h-custom {
+height: calc(100% - 73px);
+}
+@media (max-width: 450px) {
+.h-custom {
+height: 100%;
+}
+}
+</style>
--- app/src/views/SignupView.vue
+++ app/src/views/SignupView.vue
@@ -1,19 +1,53 @@
 <template>
+  <section class="vh-100">
+  <!-- Jumbotron -->
+  <div class="row d-flex justify-content-center align-items-center h-100">
     <div class="container">
-        <form class="form-signin" @submit.prevent="signup">
-            <h2 class="form-signin-heading">Create an account</h2>
-            <div v-if="err" class="alert alert-danger">
-                {{err}}
+      <div class="row gx-lg-5 align-items-center">
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <h1 class="my-5 display-3 fw-bold ls-tight">
+            Hand Sign Me <br />
+            <span class="text-primary">Sign your document</span>
+          </h1>
+          <p style="color: hsl(217, 10%, 50.8%)">
+            Signature tool to electronically sign documents.<br/>
+            Sign your own documents and send invitations to sign.
+          </p>
+        </div>
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <div class="card">
+            <div class="card-body py-5 px-md-5">
+              <div class="container">
+                  <form class="form-signin" @submit.prevent="signup">
+                      <h2 class="form-signin-heading">Create an account</h2>
+                      <div v-if="err" class="alert alert-danger">
+                          {{err}}
+                      </div>
+                      <!-- Email input -->
+                      <div class="form-outline mb-4">
+                        <input v-model="email" type="email" id="inputEmail" name="email" class="form-control" placeholder="Email" required autofocus>
+                        <label for="inputEmail" class="sr-only">Email address</label>
+                      </div>
+                      <!-- Password input -->
+                      <div class="form-outline mb-4">
+                        <input v-model="password" type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+                        <label for="inputPassword" class="sr-only">Password</label>
+                      </div>
+                      <!-- Password input -->
+                      <div class="form-outline mb-4">
+                        <input v-model="password2" type="password" id="inputPassword2" name="password2" class="form-control" placeholder="Confirm Password" required>
+                        <label for="inputPassword" class="sr-only">Confirm Password</label>
+                      </div>
+                      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+                  </form>
             </div>
-            <label for="inputEmail" class="sr-only">Email</label>
-            <input v-model="email" type="email" id="inputEmail" name="email" class="form-control" placeholder="Email" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input v-model="password" type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-            <label for="inputPassword" class="sr-only">Repeat Password</label>
-            <input v-model="password2" type="password" id="inputPassword2" name="password2" class="form-control" placeholder="Repeat Password" required>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
-        </form>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+  </div>
+</section>
 </template>
 
 <script>
